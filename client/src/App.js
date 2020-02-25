bcfb75a125f35f762f7cb05f66b1d802dc549d7a
[Brandon Teague] <[teaguebrandon@outlook.com]>
2020-02-25 17:45:45 -0500

    converted everything to use useParams and new router methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { MovieList } from "./Movies/MovieList.js";
-import { Movie } from "./Movies/Movie.js";
+import MovieList from "./Movies/MovieList.js";
+import Movie from "./Movies/Movie.js";
 import SavedList from "./Movies/SavedList";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+
+  const addToSavedList = newMovie => {
+    const exists = savedList.filter(movie => movie.id === newMovie.id)
+    !exists.length && setSavedList([...savedList, newMovie])
+  }
+
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:MID" component={Movie} />
+      <SavedList savedList={savedList} clearAll={setSavedList} />
+      <Route exact path='/'>
+        <MovieList />
+      </Route>
+      <Route path='/movies/:id'>
+        <Movie savedList={savedList} setSavedList={setSavedList} addToSavedList={addToSavedList} />
+      </Route>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,56 +1,47 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import MovieCard from './MovieCard'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
-export const Movie = props => {
-  const [movie, setMovie] = useState({});
+const Movie = ({ savedList, setSavedList, addToSavedList }) => {
+  const [movie, setMovie] = useState()
+
+  const { id } = useParams()
+
+  let deleteMovie = movieToRemove => {
+    setSavedList(savedList.filter(movie => movie.id !== movieToRemove.id))
+  }
+
+  const saveMovie = movie => {
+    addToSavedList(movie)
+  }
 
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
-        setMovie(response.data);
+        setMovie(response.data)
       })
       .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+        console.error(error)
+      })
+  }, [id])
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>Loading movie information...</div>
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
+    <div className='save-wrapper'>
+      <MovieCard movie={movie} />
+      <div className='save-button' onClick={() => saveMovie(movie)}>
+        Save
+			</div>
+      <div className='delete-button' onClick={() => deleteMovie(movie)}>
+        Delete
+			</div>
     </div>
-  );
-};
+  )
+}
 
-export default Movie;
+export default Movie
